d8f2427ad963453e6a478a71a538ddc5ac0f4009
高橋知嗣 <tomotsugu00kaiin@gmail.com>
2018-01-07 02:05:02 +0900

    イラスト追加、ボタン押して保存とキーボード収納とアラート表示

--- faveur-ios/faveur-ios/MemoViewController.swift
+++ faveur-ios/faveur-ios/MemoViewController.swift
@@ -10,7 +10,29 @@ import UIKit
 
 class MemoViewController: UIViewController {
 
+    //メモNo
+    var memoNo = ""
+    
+    //UserDefaults活性化！
+    let saves = UserDefaults.standard
+    
+//    ボタンを押したら保存してキーボード閉じる
+    @IBAction func saveButton(_ sender: Any) {
+        saves.set(memo.text, forKey: "myText"+memoNo)
+        memo.resignFirstResponder()
+        //        アラートの設定
+        let alert = UIAlertController(
+            title: "保存完了",
+            message: "保存したよ！🍞😄👍",
+            preferredStyle: .alert)
+        //          アラート
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
+    }
+    @IBOutlet weak var memo: UITextView!
+    
     override func viewDidLoad() {
+        memo.text = saves.string(forKey: "myText"+memoNo)
         super.viewDidLoad()
     }
 

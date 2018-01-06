b35f009a3d93ce1405c2589cdd7d78169e6f25fe
高橋知嗣 <tomotsugu00kaiin@gmail.com>
2018-01-07 00:49:41 +0900

    ストーリーボード上で要素を配置

---
+++ faveur-ios/faveur-ios/MemoViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MemoViewController.swift
+//  faveur-ios
+//
+//  Created by 高橋知嗣 on 2018/01/06.
+//  Copyright © 2018年 ttmtg. All rights reserved.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+
+}
